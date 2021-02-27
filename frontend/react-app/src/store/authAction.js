3acae1814dffdcfd117651320eaf1830df6ab3a2
hduongck <hduongck@DESKTOP-O4CVP7R.localdomain>
2021-02-27 19:18:31 +0700

    apply redux

--- frontend/react-app/src/store/authAction.js
+++ frontend/react-app/src/store/authAction.js
@@ -7,3 +7,8 @@ const SESSION_DURATION = settings.SESSION_DURATION
 // ##############################################
 // Contains Auth Action Functions. These perform two kinds of things:
 // 1) Return Action Objects
+// a) Simple return an action objects
+// b) Perform some action and then return an Action object
+// 2) Return a Disbatch(action) combination
+// a) Perform an action then return a Dispatch(action combination)
+// This Dispatch(Action) could be used by some other function to dispatch action to the store
